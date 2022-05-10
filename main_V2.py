69e89c6616414569235e6fef6b61c33aa6022956
sn00wden <matthieu2603@gmail.com>
2022-05-10 18:31:37 +0200

    FASTTTTTT

--- main_V2.py
+++ main_V2.py
@@ -1,15 +1,15 @@
-import csv
-import random
+
 import numpy as np
+import matplotlib.pyplot as plt
 # from matplotlib import testing
 
 def ExtractFile(path) :
     with open(path,newline='') as file :
         # global y,x
-        filereader = csv.reader(file)
-        filereader = [i[0].split(';') for i in list(filereader)]
-        filereader = [list(map(float,i)) for i in filereader]
-        random.shuffle(filereader)
+        filereader = np.genfromtxt(file,delimiter=';')
+
+        np.random.shuffle(filereader)
+        
         x = np.array([i[:-1] for i in filereader])
         y = np.array([i[-1] for i in filereader])
         return x,y
@@ -19,24 +19,23 @@ def KNN(x : np.array,y:np.array, data : np.array, accuracy=4 ) :
     
     distance = np.zeros(len(x))
 
-    for i in range(len(x)) :
-        temp = (np.sum((x[i] - data)**2)) 
-        if temp != 0 :
-            distance[i] = 1/temp
-        else : 
-            distance[i] = 100000000000000000000
+    
+        
+        
+    distance = np.sum((x - data)**2,axis=1)
+        
     
     keys = np.argsort(distance)
     # temp_x = np.take(x,keys)
     distance = np.take(distance,keys)
     temp_y = np.take(y,keys)
-    temp_y = temp_y[-accuracy:]
-    distance = distance[-accuracy:]
+    temp_y = temp_y[:accuracy]
+    distance = distance[:accuracy]
     
     
     
     # print(sum(temp_y)/accuracy)
-    return 0 if (sum(temp_y)/accuracy)<0.5 else 1
+    return 0 if (sum(temp_y)/accuracy)<=0.5 else 1
 
 
 def Resultat(dataset : tuple,prop_test :float , accuracy=4):
@@ -69,4 +68,35 @@ if __name__ == '__main__':
     # print(dataset)
     # print(type(dataset[0]))
 
-    print(Resultat(dataset,0.5,10))
+    # print(Resultat(dataset,0.8,40))
+    
+    #test all accuracy values 20 times, then make a graph
+    
+    accuracy = [i for i in range(1,50)]
+    result = np.zeros(len(accuracy))
+    for i in range(20) :
+        result_temp= np.zeros(len(accuracy))
+
+        for i in range(len(accuracy)) :
+            result_temp[i] = Resultat(dataset,0.8,i+1)
+        
+        print('end')
+        if i == 0 :
+            result = result_temp
+        else :
+            result = (result + result_temp)/2
+    
+    print()
+    plt.plot(accuracy,result)
+    key = np.argsort(result)
+    result = np.take(result,key)
+    accuracy = np.take(accuracy,key)
+    plt.show()
+
+    print("result : ",result[-1])
+    print("accuracy : ",accuracy[-1])
+
+
+
+    
+        
